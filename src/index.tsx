273e9dc1d0de5664870be767ec41c5ab2f8fcdfe
remin <noreply@remin.jp>
2020-03-16 03:00:07 +0900

    chore(package): add fontawesome

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react';
 import { render } from 'react-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
 import { App } from './App';
 import './index.scss';
+
+library.add(faPlay);
 
 render(<App />, document.getElementById('root'));
